d70cd1be4942a5dbe2bbb6a2c3a37bade204c0fb
Miguel Alvarez <malvarez@zafirus.tech>
2024-11-29 22:07:37 -0300

    change name project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Marvel_project',
+  appName: 'template_arquitecture_repository',
   webDir: 'www'
 };
 
